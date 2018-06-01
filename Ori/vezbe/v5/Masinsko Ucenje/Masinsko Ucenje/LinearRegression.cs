de8f9ea84ed17b6500a0646eee0632de6d13741b
Milos Ivankovic <ivankovicmilos5@gmail.com>
2018-06-01 08:17:44 +0200

    files

---
+++ Ori/vezbe/v5/Masinsko Ucenje/Masinsko Ucenje/LinearRegression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Masinsko_Ucenje
+{
+    public class LinearRegression
+    {
+        public double k { get; set; }
+        public double n { get; set; }
+
+	    public void fit(double[] x, double[] y) {
+            // TODO 2: implementirati fit funkciju koja odredjuje parametre k i n
+            // y = kx + n
+
+           
+            double upNum1 = 0;
+            double upNum2 = 0;
+            double upNum3 = 0;
+            double downNum1 = 0;
+        
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                upNum1 += x[i] * y[i];
+                upNum2 += x[i];
+                upNum3 += y[i];
+                downNum1 += Math.Pow(x[i], 2);
+            }
+
+            k = (x.Length * upNum1 - upNum1 * upNum2) / (x.Length * downNum1 - upNum2);
+            n = (1/x.Length)*(upNum3 - k*upNum2);        
+
+        }
+
+        public double predict(double x)
+        {   
+            // TODO 3: Implementirati funkciju predict koja na osnovu x vrednosti vraca
+            // predvinjenu vrednost y
+
+            return k*x + n;
+        }
+    }
+}

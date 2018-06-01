872ef5767b148c672335decbf0722becac8c45ae
Milos Ivankovic <ivankovicmilos5@gmail.com>
2018-06-01 08:15:32 +0200

    Files

---
+++ vezbe/v7 - Graf izracunavanja -VNM/ComputationalGraph/ComputationalGraph/SigmoidNode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ComputationalGraph
+{
+    public class SigmoidNode
+    {
+        private double x;
+        /// <summary>
+        /// Sigmoid funkcija 1/1+e^-x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private double sigmoid(double x)
+        {
+            // TODO 5.1: Implementirati forward korak sigmoidnog cvora
+            double sigm_retVal = 0.0;
+            this.x = x;
+            sigm_retVal = 1 / (1 + Math.Exp(-x));
+            return sigm_retVal;
+        }
+
+        public SigmoidNode()
+        {
+            this.x = 0;
+        }
+
+        /// <summary>
+        /// Na dobijeno x pozvati sigmoid funkciju
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public double forward(double x)
+        {
+            this.x = x;
+            return this.sigmoid(this.x);
+        }
+
+        /// <summary>
+        /// z = 1/1+e^-x
+        /// dz/dx = sigm(x)*(1-sigm(x))
+        /// =>dx = dz * sigm(x)*(1-sigm(x))
+        /// </summary>
+        /// <param name="dz"></param>
+        /// <returns></returns>
+        public double backward(double dz)
+        {
+            // TODO 5.2: Implementirati backward korak sigmoidog cvora
+            double retVal = 0.0;
+            retVal = dz * sigmoid(x) * (1 - sigmoid(x));
+
+            return retVal;
+        }
+
+        
+    }
+}
